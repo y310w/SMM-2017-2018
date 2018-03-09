232583b01f410830ca9a52bfc8d6111c6f6c944a
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-03-09 12:59:49 +0100

    ESO

---
+++ Practica 2/Practica2.Swing_GUI/src(1)/practica2/swing_gui/Practica2Swing_GUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2.swing_gui;
+
+/**
+ *
+ * @author thejoker
+ */
+public class Practica2Swing_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaPrincipal().setVisible(true);
+            }
+        });
+    }
+}
