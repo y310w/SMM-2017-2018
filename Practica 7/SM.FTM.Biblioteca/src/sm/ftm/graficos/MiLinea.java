662cb90fb34770ac63ab2f59031277fa18da790f
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-04-15 18:18:48 +0200

    Práctica 7

---
+++ Practica 7/SM.FTM.Biblioteca/src/sm/ftm/graficos/MiLinea.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sm.ftm.graficos;
+
+import java.awt.geom.Line2D;
+
+/**
+ *
+ * @author thejoker
+ */
+public class MiLinea extends Line2D.Float{
+    
+}
