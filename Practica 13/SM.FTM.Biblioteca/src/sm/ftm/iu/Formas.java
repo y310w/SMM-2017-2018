5295fdb1ca5a8a91cc5ceb840611f39b45906bb9
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-05-31 12:19:54 +0200

    Práctica 13

---
+++ Practica 13/SM.FTM.Biblioteca/src/sm/ftm/iu/Formas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sm.ftm.iu;
+
+/**
+ *
+ * @author thejoker
+ */
+public enum Formas {
+    Punto, Linea, Rectangulo, Elipse;
+}
